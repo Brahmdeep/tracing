0f5787ae2ae9df83cd6d208a42fb81a6c8f1bdf8
sbrahmde <sbrahmde@amazon.com>
2023-05-06 18:42:16 +0530

    added relevant Data

--- ms-two/src/main/java/com/devprobelms/MsTwoApplication.java
+++ ms-two/src/main/java/com/devprobelms/MsTwoApplication.java
@@ -7,10 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/*
- *  @author: DevProblems(Sarang Kumar A Tak)
- *  Youtube channel: https://youtube.com/@devproblems
- */
+
 @SpringBootApplication
 public class MsTwoApplication {
 
